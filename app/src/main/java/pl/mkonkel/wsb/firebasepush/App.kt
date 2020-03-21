7c59716f744d13f75784b66daafcb6ae0792c726
xM4ko <masif041x@gmail.com>
2020-03-21 10:47:26 +0100

    Implementation of firebase SDK, first configuration, Sending first test push

--- app/src/main/java/pl/mkonkel/wsb/firebasepush/App.kt
+++ app/src/main/java/pl/mkonkel/wsb/firebasepush/App.kt
@@ -1,10 +1,14 @@
 package pl.mkonkel.wsb.firebasepush
 
 import android.app.Application
+import com.google.firebase.iid.FirebaseInstanceId
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        FirebaseInstanceId.getInstance().InstanceId
+            .addO
 
 // TODO: Add FirebaseInstanceId Callback here
 //        It is goot to log or print the token somewhere - it will be needed later.
--- app/src/main/java/pl/mkonkel/wsb/firebasepush/MyFirebaseMessagingService.kt
+++ app/src/main/java/pl/mkonkel/wsb/firebasepush/MyFirebaseMessagingService.kt
@@ -1,14 +1,23 @@
 package pl.mkonkel.wsb.firebasepush
 
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
 
 // TODO: Implement your custom Messaging service.
-//class MyFirebaseMessagingService : FirebaseMessagingService() {
+class MyFirebaseMessagingService : FirebaseMessagingService() {
 
 //    TODO: Create the NotificationManager here
 
-//    TODO: Override the "onMessageReceived" where we will be handling the PUSH message
-
-//    TODO: Add a helper method for creating the Notification Channel
+    //    TODO: Override the "onMessageReceived" where we will be handling the PUSH message
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+        val title = remoteMessage.notification?.title // uzyj jedynie gdu nei jest null!
+        val message = remoteMessage.notification?.body
+        Log.d("MESSAGE", "title: $title / message: $message ")
+    }
+    //    TODO: Add a helper method for creating the Notification Channel
 //    You will need to provide:
 //    - Channel Id
 //    - Channel Name
@@ -27,8 +36,8 @@ package pl.mkonkel.wsb.firebasepush
 //    TODO: Add a helper method for creating the Pending Intent that will allow us to run some activity
 //    If you want to pass the notification to the Activity you must use the Extras
 
-//    companion object {
-//        const val NOTIFICATION_MESSAGE_TITLE = "message_title"
-//        const val NOTIFICATION_MESSAGE_BODY = "message_body"
-//    }
-//}
+    companion object {
+        const val NOTIFICATION_MESSAGE_TITLE = "message_title"
+        const val NOTIFICATION_MESSAGE_BODY = "message_body"
+    }
+}
